9664aaba53eaeed057f2b0f5cef45b8f917cd2a4
Akke <sirpurpleness@gmail.com>
2025-04-15 10:43:47 +0200

    added .env variables

--- src/test/rest-api.test.js
+++ src/test/rest-api.test.js
@@ -1,20 +1,24 @@
 import { beforeAll, describe, expect, test } from "vitest";
+import { config } from "dotenv";
+config();
 
 const LOGIN_URL = "https://tokenservice-jwt-2025.fly.dev/token-service/v1/request-token";
 const API_URL = "https://tokenservice-jwt-2025.fly.dev/movies";
 let jwtToken;
 
 beforeAll(async () => {
+    console.log(import.meta.env.VITE_LOGIN_PASSWORD)
     const response = await fetch(LOGIN_URL, {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            username: "axel",
-            password: "123456789"
+            username: import.meta.env.VITE_LOGIN_USERNAME,
+            password: import.meta.env.VITE_LOGIN_PASSWORD
         })
     })
 
     jwtToken = await response.text();
 });
+
