ab6db33a04982e0b3ab478fb17a2ac6490933de1
Klara Frommelin <freakyfrommelin@outlook.com>
2025-04-16 10:48:25 +0200

    Update fixture calls

--- src/test/rest-api.test.js
+++ src/test/rest-api.test.js
@@ -5,50 +5,7 @@ config();
 const LOGIN_URL = "https://tokenservice-jwt-2025.fly.dev/token-service/v1/request-token";
 const API_URL = "https://tokenservice-jwt-2025.fly.dev/movies";
 let jwtToken;
-let createdMovie;
-
-// beforeAll(async () => {
-//     const response = await fetch(LOGIN_URL, {
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json"
-//         },
-//         body: JSON.stringify({
-//             username: import.meta.env.VITE_LOGIN_USERNAME,
-//             password: import.meta.env.VITE_LOGIN_PASSWORD
-//         })
-//     })
-
-//     jwtToken = await response.text();
-// });
-
-// beforeEach(async () => {
-//     const response = await fetch(API_URL, {
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json",
-//             "Authorization": `Bearer ${jwtToken}`
-//         },
-//         body: JSON.stringify({
-//             "director": "That Fly On The Wall",
-//             "description": "It's a fly. What more do you expect?",
-//             "productionYear": 1991,
-//             "title": "The fly that flew."
-//         })
-//     });
-
-//     createdMovie = await response.json();
-// });
-
-// afterEach(async () => {
-//     await fetch(`${API_URL}/${createdMovie.id}`, {
-//         method: "DELETE",
-//         headers: {
-//             "Authorization": `Bearer ${jwtToken}`
-//         }
-//     });
-//     createdMovie = null;
-// });
+let createdMovie;   
 
 function getJWT(){
     beforeAll(async () => {
@@ -67,67 +24,75 @@ function getJWT(){
     })
 }
 
-async function addMovie(){
-    const response = await fetch(API_URL, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${jwtToken}`
-        },
-        body: JSON.stringify({
-            "director": "That Fly On The Wall",
-            "description": "It's a fly. What more do you expect?",
-            "productionYear": 1991,
-            "title": "The fly that flew."
-        })
+function addMovie(){
+    beforeEach(async () => {
+        const response = await fetch(API_URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${jwtToken}`
+            },
+            body: JSON.stringify({
+                "director": "That Fly On The Wall",
+                "description": "It's a fly. What more do you expect?",
+                "productionYear": 1991,
+                "title": "The fly that flew."
+            })
+        });
+    
+        createdMovie = await response.json()
     });
-
-    createdMovie = await response.json();
 }
 
-async function cleanUp(){
-    await fetch(`${API_URL}/${createdMovie.id}`, {
-        method: "DELETE",
-        headers: {
-            "Authorization": `Bearer ${jwtToken}`
-        }
+function cleanUp(){
+    afterEach(async () => {
+        await fetch(`${API_URL}/${createdMovie.id}`, {
+            method: "DELETE",
+            headers: {
+                "Authorization": `Bearer ${jwtToken}`
+            }
+        });
+        createdMovie = null
     });
-    createdMovie = null;
 }
 
-// describe("PUT + GET /movies", () => {
-//     test("ändra titeln", async () => {
-//         const newTitle = "ändrad titel!";
-//         createdMovie.title = newTitle;
+describe("PUT + GET /movies", () => {
+    getJWT()
+    addMovie()
+    cleanUp()
+
+    test("ändra titeln", async () => {
+        const newTitle = "ändrad titel!";
+        createdMovie.title = newTitle;
         
-//         const responsePut = await fetch(`${API_URL}/${createdMovie.id}`, {
-//             method: "PUT",
-//             headers: {
-//                 "Authorization": `Bearer ${jwtToken}`,
-//                 "Content-Type": "application/json"
-//             },
-//             body: JSON.stringify(createdMovie)
-//         });
+        const responsePut = await fetch(`${API_URL}/${createdMovie.id}`, {
+            method: "PUT",
+            headers: {
+                "Authorization": `Bearer ${jwtToken}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(createdMovie)
+        });
 
-//         expect(responsePut.status).toBe(200);
+        expect(responsePut.status).toBe(200);
 
-//         const responseGet = await fetch(`${API_URL}/${createdMovie.id}`, {
-//             method: "GET",
-//             headers: {
-//                 "Authorization": `Bearer ${jwtToken}`
-//             },
-//         });
+        const responseGet = await fetch(`${API_URL}/${createdMovie.id}`, {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${jwtToken}`
+            },
+        });
 
-//         expect(responseGet.status).toBe(200);
+        expect(responseGet.status).toBe(200);
 
-//         const responseGetData = await responseGet.json();
+        const responseGetData = await responseGet.json();
 
-//         expect(responseGetData.title).toBe(newTitle);
-//     });
-// });
+        expect(responseGetData.title).toBe(newTitle);
+    });
+});
 
 
-// //  
+// Detta test behöver läggas in i sin korrekta describe!! Ansvarig kodare får fixa till detta 
 // test("GET /movies/{id}", async () => {
 //     const response = await fetch(`${API_URL}/${createdMovie.id}`, {
 //         method: "GET",
@@ -148,7 +113,7 @@ describe('POST + DELETE /movies', () => {
     getJWT()
 
     test('namn', async() => {
-        //POST Klara
+        // POST Klara
         const response = await fetch(API_URL,{
             method: "POST",
             headers: {
@@ -166,7 +131,7 @@ describe('POST + DELETE /movies', () => {
         expect(response.status).toBe(201)
         const responseJson = await response.json()
 
-        //DELETE Andreas
+        // DELETE Andreas
         const deleteResponse = await fetch(`${API_URL}/${responseJson.id}`, {
             method: "DELETE",
             headers:  {
