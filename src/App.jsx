2a0184e36337d77f316bf995284fd74093e16c62
Akke <sirpurpleness@gmail.com>
2025-04-23 09:45:21 +0200

    added login with JWT

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
 import Counter from "./components/Counter/Counter.jsx";
+import Login from './components/Login/Login.jsx';
 
 function App() {
   return (
     <>
       count is <Counter />
+
+      <Login />
     </>
   )
 }
