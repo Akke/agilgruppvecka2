75340bfceec65e283c118e97a63ecc1888a8b6e5
andreasLoetzsch <andreas.lotzsch@gmail.com>
2025-04-23 12:57:28 +0200

    feat:  added functions to add and subtract from the counter and added 2 buttons

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -2,11 +2,24 @@ import { useState } from "react";
 
 const Counter = () => {
     const [count, setCount] = useState(0);
+    const addToCount = () => {
+        setCount(count + 1)
+    }
+    const subtractCount = () => {
+        if(count == 0){
+            return
+        }
+        setCount(count - 1)
+    } 
 
     return (
-        <>
+        <div>
+            <button onClick={subtractCount}>-</button>
             {count}
-        </>
+            <button onClick={addToCount}>+</button>
+        </div>
+            
+        
     );
 }
 
