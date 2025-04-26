62070fcd677ca2939a8da745855a989bc57f745d
MungangaThelly <daddymulongo@yahoo.fr>
2025-04-26 09:24:46 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,7 @@
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 89ce67a381ce397a90612fc5fcd5e8b6ed2bb57e
 import './App.css'
 import Counter from "./components/Counter/Counter.jsx";
 import Login from './components/Login/Login.jsx';
 
-<<<<<<< HEAD
-
-import React from 'react'
-import Raknare from './components/Raknare'
-import MovieList from './components/MovieList'
-
-
-function App() {
-  return (<>
-  
-  <h1>App</h1>
-  <Raknare/>
-  <MovieList/>
-
-
-
-  </>)
-=======
 function App() {
   return (
     <>
@@ -33,7 +10,6 @@ function App() {
       <Login />
     </>
   )
->>>>>>> 89ce67a381ce397a90612fc5fcd5e8b6ed2bb57e
 }
 
 export default App
